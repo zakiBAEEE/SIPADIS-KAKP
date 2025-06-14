9bb2c6833cbf1289fa0c6c48bb194acd1d29ff1b
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2025-06-14 23:56:25 +0700

    Tambah nav Ditolak dan Arsip Surat

--- resources/views/components/layout/nav.blade.php
+++ resources/views/components/layout/nav.blade.php
@@ -56,17 +56,7 @@
                             aria-controls="sidebarCollapseList"
                             class="flex items-center justify-between min-w-60 cursor-pointer py-1.5 px-2.5 rounded-md align-middle transition-all duration-300 ease-in text-slate-600 hover:text-slate-800 hover:bg-slate-200 focus:bg-slate-200 focus:text-slate-800">
                             <span class="flex items-center gap-3">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="1.4em" height="1.4em"
-                                    viewBox="0 0 24 24" fill="none">
-                                    <path
-                                        d="M18 6.00002V6.75002H18.75V6.00002H18ZM15.7172 2.32614L15.6111 1.58368L15.7172 2.32614ZM4.91959 3.86865L4.81353 3.12619H4.81353L4.91959 3.86865ZM5.07107 6.75002H18V5.25002H5.07107V6.75002ZM18.75 6.00002V4.30604H17.25V6.00002H18.75ZM15.6111 1.58368L4.81353 3.12619L5.02566 4.61111L15.8232 3.0686L15.6111 1.58368ZM4.81353 3.12619C3.91638 3.25435 3.25 4.0227 3.25 4.92895H4.75C4.75 4.76917 4.86749 4.63371 5.02566 4.61111L4.81353 3.12619ZM18.75 4.30604C18.75 2.63253 17.2678 1.34701 15.6111 1.58368L15.8232 3.0686C16.5763 2.96103 17.25 3.54535 17.25 4.30604H18.75ZM5.07107 5.25002C4.89375 5.25002 4.75 5.10627 4.75 4.92895H3.25C3.25 5.9347 4.06532 6.75002 5.07107 6.75002V5.25002Z"
-                                        fill="#1C274D" />
-                                    <path d="M8 12H16" stroke="#1C274D" stroke-width="1.5" stroke-linecap="round" />
-                                    <path d="M8 15.5H13.5" stroke="#1C274D" stroke-width="1.5" stroke-linecap="round" />
-                                    <path
-                                        d="M4 6V19C4 20.6569 5.34315 22 7 22H17C18.6569 22 20 20.6569 20 19V14M4 6V5M4 6H17C18.6569 6 20 7.34315 20 9V10"
-                                        stroke="#1C274D" stroke-width="1.5" stroke-linecap="round" />
-                                </svg>
+                                 <img src="{{ asset('images/email.png') }}" alt="" width="23px">
                                 Surat Masuk
                             </span>
                             <span data-icon
@@ -82,6 +72,10 @@
                         <div class="overflow-hidden transition-[max-height] duration-300 ease-in-out max-h-0"
                             id="suratmasukcollapselist">
                             <ul class="flex flex-col gap-0.5 min-w-60">
+                                 <li>
+                                    <a href=""
+                                        class="pl-10 flex items-center cursor-pointer py-1.5 px-2.5 rounded-md align-middle select-none font-sans transition-all duration-300 ease-in bg-transparent text-slate-600 hover:text-slate-800 hover:bg-slate-200 focus:bg-slate-200 focus:text-slate-800">Ditolak</a>
+                                </li>
                                 <li>
                                     <a href="{{ route('surat.denganDisposisi') }}"
                                         class="pl-10 flex items-center cursor-pointer py-1.5 px-2.5 rounded-md align-middle select-none font-sans transition-all duration-300 ease-in bg-transparent text-slate-600 hover:text-slate-800 hover:bg-slate-200 focus:bg-slate-200 focus:text-slate-800">Terkirim</a>
@@ -131,6 +125,16 @@
                                 </li>
                             </ul>
                         </div>
+                    </li>
+
+                      <li>
+                        <a href=""
+                            class="flex items-center py-1.5 px-2.5 rounded-md align-middle select-none font-sans transition-all duration-300 ease-in aria-disabled:opacity-50 aria-disabled:pointer-events-none bg-transparent text-slate-600 hover:text-slate-800 hover:bg-slate-200 focus:bg-slate-200 focus:text-slate-800">
+                            <span class="grid place-items-center shrink-0 me-2.5">
+                                 <img src="{{ asset('images/inbox.png') }}" alt=""  width="23px">
+                            </span>
+                            Arsip Surat
+                        </a>
                     </li>
 
                     <hr class="-mx-3 my-3 border-slate-200" />
