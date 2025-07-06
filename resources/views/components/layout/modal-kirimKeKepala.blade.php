9bebb8d99c54e88d23bfd2eb831e78e0c6c4d8de
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2025-07-06 12:01:34 +0700

    Create modal kirim ke kepala

--- resources/views/components/layout/modal-kirimKeKepala.blade.php
+++ resources/views/components/layout/modal-kirimKeKepala.blade.php
@@ -1,10 +1,9 @@
-
 <div class="flex justify-center">
-    <button type="button" data-toggle="modal" data-target="#modalTambahDisposisi"
+    <button type="button" data-toggle="modal" data-target="#modalKirimKeKepala"
         class="inline-flex border font-sans font-medium text-center transition-all duration-300 ease-in disabled:opacity-50 disabled:shadow-none disabled:cursor-not-allowed data-[shape=pill]:rounded-full data-[width=full]:w-full focus:shadow-none text-sm rounded-md py-1 px-2 shadow-sm hover:shadow bg-green-500 border-slate-300 text-slate-50 hover:bg-green-700 hover:border-slate-700">
-        Disposisikan</button>
+        Kirim Ke Kepala</button>
     <div class="fixed antialiased inset-0 bg-slate-950/50 flex justify-center items-center opacity-0 pointer-events-none transition-opacity duration-300 ease-out z-[9999]"
-        id="modalTambahDisposisi" aria-hidden="true">
+        id="modalKirimKeKepala" aria-hidden="true">
         <div
             class="bg-white rounded-lg w-10/12 md:w-8/12 lg:w-6/12 transition-transform duration-300 ease-out scale-100">
             <div class="p-4 pb-2 flex justify-between items-center">
--- resources/views/pages/super-admin/detail-surat.blade.php
+++ resources/views/pages/super-admin/detail-surat.blade.php
@@ -194,19 +194,7 @@
                                 @endif
 
                                 @if ($isAdmin && ($belumPernahDidisposisikan || $terakhirKembalikan))
-                                    <form method="POST" action="{{ route('surat.kirimKeKepala', $surat->id) }}"
-                                        onsubmit="return confirm('Anda yakin ingin mengirim ulang surat ini setelah direvisi?');">
-                                        @csrf
-                                        <button type="submit"
-                                            class="px-3 py-1 bg-green-600 text-white rounded-md text-xs font-semibold hover:bg-green-700 flex flex-row items-center gap-1">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                    d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-                                            </svg>
-                                            <p>Kirim ke Kepala</p>
-                                        </button>
-                                    </form>
+                                    @include('components.layout.modal-kirimKeKepala')
                                 @endif
 
                                 {{-- Tombol untuk mengembalikan disposisi (jika sudah dibuat) --}}
