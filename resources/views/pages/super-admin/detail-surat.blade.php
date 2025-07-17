a677127123629edf61ba0c139963d226041da970
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2025-07-18 00:02:40 +0700

    Minor Changes

--- resources/views/pages/super-admin/detail-surat.blade.php
+++ resources/views/pages/super-admin/detail-surat.blade.php
@@ -203,13 +203,12 @@
                                             frameborder="0"></iframe>
                                     @else
                                         <div class="flex flex-col items-start gap-2 mt-4">
-                                            <img src="{{ asset('storage/' . $surat->file_path) }}" alt="Preview Dokumen"
-                                                class="max-w-full h-auto border rounded">
-
                                             <a href="{{ asset('storage/' . $surat->file_path) }}" download
-                                                class="px-4 py-2 text-sm bg-slate-700 text-white rounded hover:bg-slate-800 transition">
+                                                class="px-2 py-2 text-sm bg-slate-700 text-white rounded hover:bg-slate-800 transition">
                                                 Download Gambar
                                             </a>
+                                            <img src="{{ asset('storage/' . $surat->file_path) }}" alt="Preview Dokumen"
+                                                class="max-w-full h-auto border rounded">
                                         </div>
                                     @endif
                                 </div>
