f61f07071c234f20398262ce53b46ee97f61608b
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2025-07-17 22:11:07 +0700

    Minor Changes

--- resources/views/components/base/input-email.blade.php
+++ resources/views/components/base/input-email.blade.php
@@ -7,7 +7,7 @@
 
 <div class="flex flex-col gap-1 w-full">
     <label for="{{ $name }}" class="text-sm text-slate-700 font-semibold">{{ $label }}</label>
-    <input name="{{ $name }}" type="email" id="{{ $name }}" placeholder="{{ $placeholder }}"
+    <input name="{{ $name }}" id="{{ $name }}" placeholder="{{ $placeholder }}"
         value="{{ old($name, $value) }}"
         class="px-4 py-2 rounded-md border text-sm focus:ring-2 focus:ring-slate-400 focus:outline-none
                @error($name) border-red-500 bg-red-50 @else border-slate-300 @enderror">
