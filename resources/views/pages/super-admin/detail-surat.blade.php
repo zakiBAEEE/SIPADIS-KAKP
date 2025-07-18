81cdbd5069287a0f2218fbdbf138d866f981c042
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2025-07-18 17:43:23 +0700

    Styling Detail Surat

--- resources/views/pages/super-admin/detail-surat.blade.php
+++ resources/views/pages/super-admin/detail-surat.blade.php
@@ -112,38 +112,82 @@
 
 
                 <div class="p-4">
-                    @include('components.table.table-detail-surat', [
-                        'surat' => $surat,
-                    ])
-                    <div class="text-center">
-                        <button data-toggle="collapse" data-target="#collapseButton" aria-expanded="false"
-                            aria-controls="collapseButton"
-                            class="inline-flex items-center justify-center border align-middle select-none font-sans font-medium text-center transition-all duration-300 ease-in disabled:opacity-50 disabled:shadow-none disabled:cursor-not-allowed data-[shape=pill]:rounded-full data-[width=full]:w-full focus:shadow-none text-sm rounded-md py-2 px-4 shadow-sm hover:shadow-md bg-slate-800 border-slate-800 text-slate-50 hover:bg-slate-700 hover:border-slate-700">Lampiran
-                            File</button>
-                        <div class="overflow-hidden transition-[max-height] duration-300 ease-in-out max-h-0 mt-1"
-                            id="collapseButton">
+                    <div class="flex flex-col gap-2">
+                        <div class="flex flex-row items-center justify-start gap-3 mb-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" viewBox="0 0 24 24"
+                                fill="none">
+                                <path stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M11 5h10M11 9h5" />
+                                <rect width="4" height="4" x="3" y="5" stroke="#000000" stroke-linecap="round"
+                                    stroke-linejoin="round" stroke-width="2" rx="1" />
+                                <path stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M11 15h10m-10 4h5" />
+                                <rect width="4" height="4" x="3" y="15" stroke="#000000" stroke-linecap="round"
+                                    stroke-linejoin="round" stroke-width="2" rx="1" />
+                            </svg>
+                            <h2 class="text-2xl font-bold text-slate-800">Data Surat</h2>
+                        </div>
 
-                            <div class="space-y-1.5">
-                                <p class="font-sans text-sm text-slate-800 font-bold mb-2">Preview Dokumen:</p>
-                                @if ($surat->file_path)
-                                    <div class="mt-4">
-                                        @if (Str::endsWith($surat->file_path, '.pdf'))
-                                            <iframe src="{{ asset('storage/' . $surat->file_path) }}"
-                                                class="w-full h-[500px]" frameborder="0"></iframe>
-                                        @else
-                                            <div class="flex flex-col items-start gap-2 mt-4">
-                                                <a href="{{ asset('storage/' . $surat->file_path) }}" download
-                                                    class="px-2 py-2 text-sm bg-slate-700 text-white rounded hover:bg-slate-800 transition">
-                                                    Download Gambar
-                                                </a>
-                                                <img src="{{ asset('storage/' . $surat->file_path) }}" alt="Preview Dokumen"
-                                                    class="max-w-full h-auto border rounded">
-                                            </div>
-                                        @endif
-                                    </div>
-                                @else
-                                    <p class="text-sm text-slate-600">Tidak ada dokumen terlampir.</p>
-                                @endif
+                        @include('components.table.table-detail-surat', [
+                            'surat' => $surat,
+                        ])
+                    </div>
+
+                    <div class="flex flex-col gap-2 mt-7">
+                        <div class="flex flex-row items-center justify-start gap-3 mb-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                                xmlns:sketch="http://www.bohemiancoding.com/sketch/ns" width="30px" height="30px"
+                                viewBox="-8 0 32 32" version="1.1">
+
+                                <title>attachment</title>
+                                <desc>Created with Sketch Beta.</desc>
+                                <defs>
+
+                                </defs>
+                                <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"
+                                    sketch:type="MSPage">
+                                    <g id="Icon-Set" sketch:type="MSLayerGroup"
+                                        transform="translate(-212.000000, -151.000000)" fill="#000000">
+                                        <path
+                                            d="M226,155 L226,175 C226,178.313 223.313,181 220,181 C216.687,181 214,178.313 214,175 L214,157 C214,154.791 215.791,153 218,153 C220.209,153 222,154.791 222,157 L222,175 C222,176.104 221.104,177 220,177 C218.896,177 218,176.104 218,175 L218,159 L216,159 L216,175 C216,177.209 217.791,179 220,179 C222.209,179 224,177.209 224,175 L224,157 C224,153.687 221.313,151 218,151 C214.687,151 212,153.687 212,157 L212,176 C212.493,179.945 215.921,183 220,183 C224.079,183 227.507,179.945 228,176 L228,155 L226,155"
+                                            id="attachment" sketch:type="MSShapeGroup">
+
+                                        </path>
+                                    </g>
+                                </g>
+                            </svg>
+                            <h2 class="text-2xl font-bold text-slate-800">Lampiran Surat</h2>
+                        </div>
+                        <div class="text-center">
+                            <button data-toggle="collapse" data-target="#collapseButton" aria-expanded="false"
+                                aria-controls="collapseButton"
+                                class="inline-flex items-center justify-center border align-middle select-none font-sans font-medium text-center transition-all duration-300 ease-in disabled:opacity-50 disabled:shadow-none disabled:cursor-not-allowed data-[shape=pill]:rounded-full data-[width=full]:w-full focus:shadow-none text-sm rounded-md py-2 px-4 shadow-sm hover:shadow-md bg-slate-800 border-slate-800 text-slate-50 hover:bg-slate-700 hover:border-slate-700">Lampiran
+                                File</button>
+                            <div class="overflow-hidden transition-[max-height] duration-300 ease-in-out max-h-0 mt-1"
+                                id="collapseButton">
+
+                                <div class="space-y-1.5">
+                                    <p class="font-sans text-sm text-slate-800 font-bold mb-2">Preview Dokumen:</p>
+                                    @if ($surat->file_path)
+                                        <div class="mt-4">
+                                            @if (Str::endsWith($surat->file_path, '.pdf'))
+                                                <iframe src="{{ asset('storage/' . $surat->file_path) }}"
+                                                    class="w-full h-[500px]" frameborder="0"></iframe>
+                                            @else
+                                                <div class="flex flex-col items-start gap-2 mt-4">
+                                                    <a href="{{ asset('storage/' . $surat->file_path) }}" download
+                                                        class="px-2 py-2 text-sm bg-slate-700 text-white rounded hover:bg-slate-800 transition">
+                                                        Download Gambar
+                                                    </a>
+                                                    <img src="{{ asset('storage/' . $surat->file_path) }}"
+                                                        alt="Preview Dokumen" class="max-w-full h-auto border rounded">
+                                                </div>
+                                            @endif
+                                        </div>
+                                    @else
+                                        <p class="text-sm text-slate-600">Tidak ada dokumen terlampir.</p>
+                                    @endif
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -154,7 +198,21 @@
 
 
                 <div x-data="{ tab: 'timeline' }" class="p-4">
-                    <h2 class="text-xl font-semibold text-slate-800 mb-4">Progres Disposisi</h2>
+                    <div class="flex flex-row items-center justify-start gap-3 mb-5">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor">
+                            <circle cx="4" cy="12" r="2" stroke="currentColor" stroke-width="2" />
+                            <circle cx="12" cy="12" r="2" stroke="currentColor" stroke-width="2" />
+                            <circle cx="20" cy="12" r="2" stroke="currentColor" stroke-width="2" />
+                            <line x1="6" y1="12" x2="10" y2="12" stroke="currentColor"
+                                stroke-width="2" />
+                            <line x1="14" y1="12" x2="18" y2="12" stroke="currentColor"
+                                stroke-width="2" />
+                        </svg>
+
+                        <h2 class="text-2xl font-bold text-slate-800">Progres Disposisi</h2>
+                    </div>
+
 
                     {{-- Tombol Tab --}}
                     <div class="flex space-x-2 mb-4">
